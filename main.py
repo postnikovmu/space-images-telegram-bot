1119df4c29c97fbf29c733fc316f2e4b3edb791b
Mikhail Postnikov <postnikovmu@mail.ru>
2024-06-10 09:55:29 +0300

    get spacex links

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from pathlib import Path
 import os
 
 
+def get_spacex_links(launch_id='latest'):
+    response = requests.get(f"https://api.spacexdata.com/v5/launches/{launch_id}")
+    response.raise_for_status()
+    return response.json()['links']['flickr']['original']
+
+
 def download_image(url, file_name, dir_name):
     Path(dir_name).mkdir(parents=True, exist_ok=True)
     file_path = os.path.join(dir_name, file_name)
@@ -16,7 +22,9 @@ def main():
     file_name = 'image.jpg'
     dir_name = './images'
 
-    download_image(url=image_url, file_name=file_name, dir_name=dir_name)
+    # download_image(url=image_url, file_name=file_name, dir_name=dir_name)
+
+    print(get_spacex_links('5eb87d47ffd86e000604b38a'))
 
 
 if __name__ == '__main__':

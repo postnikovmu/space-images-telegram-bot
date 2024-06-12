d9cf1dc37ed5688afacb14c44a1b56159bb398bd
Mikhail Postnikov <postnikovmu@mail.ru>
2024-06-12 13:30:12 +0300

    get a lot of nasa images

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from urllib.parse import urlsplit, unquote
 from os.path import split, splitext
 import os
+from dotenv import load_dotenv
 
 
 def get_spacex_links(launch_id='latest'):
@@ -11,22 +12,22 @@ def get_spacex_links(launch_id='latest'):
     return response.json()['links']['flickr']['original']
 
 
-def download_image(url, file_name, dir_name):
+def download_image(url, file_name, dir_name, extension='.jpg'):
+    full_file_name = file_name + extension
     Path(dir_name).mkdir(parents=True, exist_ok=True)
-    file_path = os.path.join(dir_name, file_name)
+    file_path = os.path.join(dir_name, full_file_name)
     response = requests.get(url)
+    response.raise_for_status()
     with open(file_path, 'wb') as file:
         file.write(response.content)
 
 
 def fetch_spacex_last_launch(launch_id='latest'):
-    dir_name = './images'
-
     spacex_links = get_spacex_links(launch_id)
     for link_number, link in enumerate(spacex_links):
-        file_name = 'spacex_image_' + str(link_number) + '.jpg'
-        download_image(link, file_name, dir_name)
-        print( link_number, link)
+        file_name = 'spacex_image_' + str(link_number)
+        download_image(link, file_name, './images', '.jpg')
+        print(link_number, link)
 
 
 def get_link_image_extension(link):
@@ -36,12 +37,32 @@ def get_link_image_extension(link):
     return extension
 
 
+def fetch_nasa_images(count):
+    load_dotenv()
+    token = os.environ['NASA_TOKEN']
+    params = {
+        'api_key': token,
+    }
+    if count:
+        params.update({'count': count})
+    response = requests.get("https://api.nasa.gov/planetary/apod", params=params)
+    response.raise_for_status()
+    response_data = response.json()
+    nasa_links = []
+    if count:
+        for row in response_data:
+            nasa_links.append(row['url'])
+    else:
+        nasa_links.append(response_data['url'])
+    for link_number, link in enumerate(nasa_links):
+        file_name = 'nasa_image_' + str(link_number)
+        download_image(link, file_name, './images', get_link_image_extension(link))
+        print(link_number, link)
+
+
 def main():
-    # fetch_spacex_last_launch('5eb87d47ffd86e000604b38a')
-    nasa_link = "https://example.com/txt/hello%20world.txt?v=9#python"
-    # nasa_link = "https://apod.nasa.gov/apod/image/2406/LionNeb_Badr_960.jpg"
-    extension = get_link_image_extension(nasa_link)
-    print(extension)
+    fetch_spacex_last_launch('5eb87d47ffd86e000604b38a')
+    fetch_nasa_images(2)
 
 
 if __name__ == '__main__':

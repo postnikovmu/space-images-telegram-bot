3dfc11f06c8d9b2de907950fd2f56a7844e4973c
Mikhail Postnikov <postnikovmu@mail.ru>
2024-06-13 14:01:49 +0300

    get nasa epic links

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from urllib.parse import urlsplit, unquote
 from os.path import split, splitext
 import os
+import datetime
 from dotenv import load_dotenv
 
 
@@ -37,9 +38,7 @@ def get_link_image_extension(link):
     return extension
 
 
-def fetch_nasa_images(count):
-    load_dotenv()
-    token = os.environ['NASA_TOKEN']
+def fetch_nasa_images(token, count):
     params = {
         'api_key': token,
     }
@@ -60,11 +59,38 @@ def fetch_nasa_images(count):
         print(link_number, link)
 
 
+def get_nasa_token():
+    load_dotenv()
+    token = os.environ['NASA_TOKEN']
+    return token
+
+
+def fetch_nasa_epic_images(token, date):
+    params = {
+        'api_key': token,
+    }
+    response = requests.get(f"https://api.nasa.gov/EPIC/api/natural/date/{date}", params=params)
+    response.raise_for_status()
+    response_data = response.json()
+    nasa_links = []
+    for row in response_data:
+        image_name = row.get('image')
+        image_item_date = row.get('date')
+        image_datetime = datetime.datetime.strptime(image_item_date, '%Y-%m-%d %H:%M:%S')
+        image_date = image_datetime.date()
+        str_image_date = image_date.strftime('%Y/%m/%d')
+        nasa_link = f"https://api.nasa.gov/EPIC/archive/natural/{str_image_date}/png/{image_name}.png"
+        nasa_links.append(nasa_link)
+
+    return None
+
+
 def main():
-    fetch_spacex_last_launch('5eb87d47ffd86e000604b38a')
-    fetch_nasa_images(2)
+    nasa_token = get_nasa_token()
+    # fetch_spacex_last_launch('5eb87d47ffd86e000604b38a')
+    # fetch_nasa_images(count=2, token=nasa_token)
+    fetch_nasa_epic_images(token=nasa_token, date='2019-05-30')
 
 
 if __name__ == '__main__':
     main()
-    

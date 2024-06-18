bd1e6488afd9dd588f54f432359a84e5d3b1db13
Mikhail Postnikov <postnikovmu@mail.ru>
2024-06-18 14:34:19 +0300

    refactor publish text

--- bot.py
+++ bot.py
@@ -3,12 +3,21 @@ import os
 from dotenv import load_dotenv
 
 
-def main():
+def get_bot_and_chat():
     load_dotenv()
     token = os.environ['TG_BOT_TOKEN']
     chat_id = os.environ['TG_CHANNEL_ID']
     bot = telegram.Bot(token=token)
-    bot.send_message(text='Hi from Alec!', chat_id=chat_id)
+    return bot, chat_id
+
+
+def bot_send_message(bot, chat_id, text):
+    bot.send_message(text=text, chat_id=chat_id)
+
+
+def main():
+    bot, chat_id = get_bot_and_chat()
+    bot_send_message(bot, chat_id, 'Hello')
 
 
 if __name__ == '__main__':
